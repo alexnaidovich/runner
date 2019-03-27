eb9b1b391bc14c297dc8691b9bc75d08dd0bf001
unknown <smiler.work@gmail.com>
2019-03-27 15:28:54 +0300

    add: settings imports, plan

--- src/config-dependencies.js
+++ src/config-dependencies.js
@@ -1,8 +1,30 @@
 const inquirer = require('inquirer');
 const { 
+  author,
+  examplePackageJson,
   isDevDepsCached,
-  devDepsCachePaths
+  devDepsCachePaths,
+  devDepsKits,
+  depsKits
 } = require('../settings.json');
+
+// Plan: 
+// 1. Define author by default
+//   1.1. Select from list of default authors or 'input manually'
+//   1.2. If 'input manually' - offer to store in defaults.
+// 2. Give name and description to the project
+// 3. Define devDeps
+//   3.1. Define if they are cahced or not 
+//     3.1.1. If devDeps are cahced, choose directory to install from (with 'input manually' value and offer to store)
+//     3.1.2. If not, go to 3.2
+//   3.2. Select devDeps kit to install (with 'input manually' value)
+//     3.2.1. If inputted manually, offer to save input as a devDeps kit
+// 4. Define deps
+//   4.1. Select deps kit to install (with 'input manually' value)
+//     4.1.1. If inputted manually, offer to save input as a deps kit
+// 5. Save 'package.json'
+// 6. Perform 'npm install'
+
 
 const MAIN_QUESTIONS = [
   {
