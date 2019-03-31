a7bf2902847188abca858236f24b269d3fba38ab
alexnaidovich <smiler.work@gmail.com>
2019-03-31 23:06:02 +0300

    rm: unused dependencies

--- index.js
+++ index.js
@@ -1,30 +1,19 @@
 #!/usr/bin/env node
 // https://codeburst.io/building-a-node-js-interactive-cli-3cb80ed76c86
 
-const inquirer = require('inquirer');
 const chalk = require('chalk');
-const shell = require('shelljs');
-const fs = require('fs');
 const path = require('path');
 
 // additions
-const { exec } = require('child_process');
-const dirname = __dirname;
-const dir_lib = path.resolve(dirname, 'lib');
-const dir_config = path.resolve(dirname, 'config');
+const dir_lib = path.resolve(__dirname, 'lib');
+const dir_config = path.resolve(__dirname, 'config');
 
 // components
 const [
-  helpers, intro, configDependencies
+  intro, configDependencies
 ] = [
-  'helpers', 'intro', 'config-dependencies'
+  'intro', 'config-dependencies'
 ].map(component => require(path.join(dir_lib, component)));
-
-const { 
-  rewriteSettings,
-  npmInit,
-  npmInstall
-} = helpers;
 
 (async function main() {
 
@@ -32,7 +21,7 @@ const {
   intro();
 
   // configure dependencies
-  const test = await configDependencies(dirname, dir_config);
+  const test = await configDependencies(__dirname, dir_config);
   console.log(
     chalk.cyan(test)
   );
